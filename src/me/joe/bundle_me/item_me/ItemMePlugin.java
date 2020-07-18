8508eea25beb2426e6bf1ba97be1f1a85bff95d1
Switch <32131859+SwitchGM@users.noreply.github.com>
2020-07-19 00:11:02 +0100

    initial commit

---
+++ src/me/joe/bundle_me/item_me/ItemMePlugin.java
@@ -0,0 +1,11 @@
+package me.joe.bundle_me.item_me;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ItemMePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("nothing much to see here yet");
+    }
+}
