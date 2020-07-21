be11cb759e658e7ade70759a9aaba21cacc1475f
Switch <32131859+SwitchGM@users.noreply.github.com>
2020-07-21 13:53:50 +0100

    updated item manager saving and loading item stacks

--- src/me/joe/bundle_me/item_me/ItemMeCommands.java
+++ src/me/joe/bundle_me/item_me/ItemMeCommands.java
@@ -1,25 +1,33 @@
 package me.joe.bundle_me.item_me;
 
+import me.joe.bundle_me.item_me.items.ItemManager;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 public class ItemMeCommands implements CommandExecutor {
 
     private ItemMePlugin plugin;
+    private ItemManager manager;
 
     public ItemMeCommands(ItemMePlugin plugin) {
         this.plugin = plugin;
+        this.manager = new ItemManager(plugin);
     }
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        ItemStack item = ((Player) commandSender).getInventory().getItemInHand();
+//        ItemStack item = ((Player) commandSender).getInventory().getItemInMainHand();
 
-        this.plugin.getConfig().set(item.getType().name(), item.serialize());
-        this.plugin.saveConfig();
+//        this.manager.save(item.getType().name(), item);
+
+        ConfigurationSection itemInfo = this.plugin.getConfig().getConfigurationSection("DIAMOND_SWORD");
+        ItemStack item = this.manager.load(itemInfo);
+
+        ((Player) commandSender).getInventory().addItem(item);
 
         return false;
     }
